d2cc724b59c9fdb32b0a0200883d7fda400827d1
yorchhh10 <00072325@uca.edu.sv>
2026-01-13 22:01:12 -0600

    add: Se agrega el menu

---
+++ calculadora.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int opcion;
+    float a, b;
+
+    do {
+        cout << "\n--- CALCULADORA ---\n";
+        cout << "1. Sumar\n";
+        cout << "2. Restar\n";
+        cout << "3. Multiplicar\n";
+        cout << "4. Dividir\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        if (opcion >= 1 && opcion <= 4) {
+            cout << "Ingrese dos numeros: ";
+            cin >> a >> b;
+        }
+
+        switch (opcion) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+
+                break;
+            case 5:
+                cout << "Saliendo...\n";
+                break;
+            default:
+                cout << "Opcion invalida\n";
+                break;
+        }
+
+    } while (opcion != 5);
+
+    return 0;
+}
